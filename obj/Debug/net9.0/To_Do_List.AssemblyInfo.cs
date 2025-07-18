5cca50496b7526c5c9bc05715183c88217dc8935
adampoewilson <adam62385@gmail.com>
2025-07-18 01:43:33 -0400

    added gitignore

--- obj/Debug/net9.0/To_Do_List.AssemblyInfo.cs
+++ obj/Debug/net9.0/To_Do_List.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To_Do_List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+078d9f2814126f853b870cb9becbff9d7fd9eda2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f460c0a020f75c0e3e498c30c88e7260f489d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("To_Do_List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To_Do_List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
